af6b468855bd928a4e41ce7de864de1237b9ae74
Avan <paperweightcode@gmail.com>
2022-09-25 21:46:29 +1000

    Fix bug

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
 var vg_1 = "map.vg.json"; 
 
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#bar-chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
