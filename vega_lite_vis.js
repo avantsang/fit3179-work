8c7c14a3de99d913b754178f592fcf5990d4b940
Avan <paperweightcode@gmail.com>
2022-10-12 13:52:57 +1100

    Upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,20 @@
 var vg_1 = "map.vg.json"; 
+var vg_2 = "bar.vg.json"; 
+var vg_3 = "line.vg.json"; 
+var vg_4 = "stacked.vg.json"; 
 
 vegaEmbed("#bar-chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar-chart2", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar-chart3", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar-chart4", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
